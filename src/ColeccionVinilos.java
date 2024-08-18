55bdedc8ead6addd49b0139d8657cf77b788f0d4
Joaquin Villa <j.villa04@ufromail.cl>
2024-08-18 18:12:23 -0400

    Add files via upload
    Version preliminar

---
+++ src/ColeccionVinilos.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class ColeccionVinilos {
+    public static void main() {
+
+        String vinilos[][] = new String[100][3];
+        System.out.println("Gestor de vinilos");
+        System.out.println("Selecciona la opción que quieras ejecutar.");
+    }
+    public static int totalVinilos(String v[][]){
+        return v.length;
+    }
+    public static String [][] agregarVinilo(v[][], String grupo, String disco, String agno){
+        for (i = 0; i < 100; i++);{
+
+        }
+    }
+    public static int disponible(String v[][]){
+        for (i = 0; i < 100; i++);{
+            if (v[i].length>=1){
+            }
+        }
+    }
+}
